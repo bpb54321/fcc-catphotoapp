d6ce4fe073e396527963ee508fa6043fae21c392
Brian Blosser <bpb54321@gmail.com>
2018-03-26 21:41:16 -0400

    Set up BeforeAll and AfterAll.

--- features/cat-photo-html.feature
+++ features/cat-photo-html.feature
@@ -1,10 +1,3 @@
-Feature: Addition
-  Scenario: 1 + 0
-    Given I start with 1
-    When I add 0
-    Then I end up with 1
-
-  Scenario: 1 + 1
-    Given I start with 1
-    When I add 1
-    Then I end up with 2
+Feature: Catphoto App
+  Scenario: Primary Heading Is Present
+    Then the element primary_heading is present
--- steps/math.js
+++ steps/math.js
@@ -1,18 +1,58 @@
+// Cucumber
 const Cucumber = require('cucumber');
 const setWorldConstructor = Cucumber.setWorldConstructor;
 const Given = Cucumber.Given;
 const When = Cucumber.When;
 const Then = Cucumber.Then;
+const BeforeAll = Cucumber.BeforeAll;
+const AfterAll = Cucumber.AfterAll;
+
+// Selenium Webdriver
+const webdriver = require('selenium-webdriver');
+const By = webdriver.By;
+const until = webdriver.until;
+const Key = webdriver.Key;
+
+// Chai
 const expect = require('chai').expect;
 
-// Define World Class
+// Define World Class (provides the 'this' in all Cucumber steps)
 class World {
   constructor() {
     this.value = 0;
+    this.driver = undefined;
   }
 }
 
 setWorldConstructor(World);
+
+BeforeAll( async function () {
+  let builder = new webdriver.Builder().forBrowser('chrome');
+  this.driver = await builder.build();
+  await this.driver.get('file:///Users/brianblosser/Sites/freecodecamp/catphotoapp/index.html');
+
+  let screen_sizes = {
+    'xs': {
+      width: 320,
+      height: 600,
+    },
+  };
+  let screen_size = screen_sizes['xs'];
+    
+  await this.driver.manage().window().setRect({
+    width: screen_size.width,
+    height: screen_size.height,
+  });
+
+  this.page = {
+    element_locators: {
+      primary_heading: '.primary-heading',
+      body_paragraph: '.body-paragraph',
+    },
+    group_locators: {
+    },
+  };
+});
 
 Given('I start with {int}', function (int) {
   this.value = int;
@@ -22,6 +62,12 @@ When('I add {int}', function (int) {
   this.value = this.value + int;
 });
 
-Then('I end up with {int}', function (int) {
-  expect(this.value).to.equal(int);
+Then('the element {word} is present', async function (element_name) {
+  let web_element = await this.driver.findElement(By.css(this.page.element_locators[element_name]));
+  this.page[element_name] = web_element;
+  expect(this.page[element_name]).to.not.be.undefined;
+});
+
+AfterAll( async function () {
+  await this.driver.quit();
 });
